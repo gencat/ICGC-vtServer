029a7938ecc3e1b2811ef7f3ad10cacb0b3f0239
bolo <bolosig@gmail.com>
2018-10-17 15:17:06 +0200

    primer commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ICGC_vtServer
+# ICGC-vtServer
 servidor expressjs que sirve VT directamente desde PostGIS
+
+La BD de PostGIS es quien se encarga de hacer la magia. El código de la BD está en https://github.com/gencat/ICGC-vt-postgis
+
+Para configurar la varibles de entorno de la conexión de la BD crear un archivo llamado **.env** en el directorio del proyecto y agregar el siguiente código:
+
+	DB_USER=${TU_USUARIO}
+	DB_HOST=${TU_HOST}
+	DB_DATABASE=${TU_BASE_DE_DATOS}
+	DB_PASS=${TU_CONTRASEÑA}
+	DB_PORT=${TU_PUERTO}
---
+++ vtServer.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+const { Pool, Client } = require('pg');
+const express = require('express');
+const app = express();
+const cluster = require('cluster');
+const cors = require('cors');
+//const router = express.Router();
+
+const config = {
+	user: process.env.DB_USER || 'postgres',
+	host: process.env.DB_HOST || 'localhost',
+	database: process.env.DB_DATABASE || 'postgres',
+	password: process.env.DB_PASS || 'postgres',
+	port: process.env.DB_PORT || 5432,
+};
+
+const pool = new Pool(config);
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+
+	// Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+	}
+
+	// Listen for dying workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker, we're not sentimental
+        console.log('Worker %d died :(', worker.id);
+        cluster.fork();
+
+    });
+	
+// Code to run if we're in a worker process
+} else {	
+
+	//app.use('/tiles', router);
+
+	app.use(cors());
+	app.use(express.static('static'));
+
+	app.get('/', function (req, res) {
+		res.send('Hello World!');
+	});
+	
+	/* GET /tiles/:z/:x/:y.mvt */
+	/* Retreive a vector tile by tileid */
+	app.get('/:z/:x/:y.pbf', async (req, res) => {
+		let { z, x, y } = req.params;
+
+		//res.send(`SELECT tile_pbf(${z}, ${x}, ${y})`);
+		//convert to TMS
+		//y = Math.pow(2, z) - y - 1;
+
+		//console.log(`SELECT icgc_vt.tile_pbf(${z}, ${x}, ${y})`);
+
+		const SQL = `SELECT icgc_vt.tile_pbf(${z}, ${x}, ${y})`;
+
+		res.setHeader('Content-Type', 'application/x-protobuf');
+		res.setHeader('Content-Type', 'application/x-protobuf');
+
+		pool.query(SQL, (err, result) => {
+			//console.log(err, result)
+			//pool.end()
+			res.send(result.rows[0].tile_pbf);
+		});
+	});
+
+	app.listen(3000, function () {
+		console.log('Example app listening on port 3000!');
+	});
+}
